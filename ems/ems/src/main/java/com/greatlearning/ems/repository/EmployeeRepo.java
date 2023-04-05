b618619b2182edf3039f55a29bd8201e9cfc30a7
ayan-neel <ayanneel9126@gmail.com>
2023-04-05 18:37:02 +0530

    delete

--- ems/ems/src/main/java/com/greatlearning/ems/repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.ems.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greatlearning.ems.model.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee,Long>{
-
-}
